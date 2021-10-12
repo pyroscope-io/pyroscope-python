17e653cba38b307afdc786e4047ed932569cea85
kurylak <adrian.kurylak@gmail.com>
2021-10-13 00:38:47 +0200

    Resolving conflicting names

--- test_wheel.py
+++ test_wheel.py
@@ -3,20 +3,20 @@ import unittest
 class TestWheel(unittest.TestCase):
     def test_import(self):
         try:
-            import pyroscope_io as pyroscope
+            import pyroscope_io as pyro
         except ImportError:
             self.fail("Unable to import pyroscope_io")
 
     def test_content(self):
         expected_functions = ["build_summary", "change_name", "configure", "remove_tags", "stop", "tag", "tag_wrapper", "test_logger"]
-        import pyroscope_io as pyroscope
-        content = dir(pyroscope)
+        import pyroscope_io as pyro
+        content = dir(pyro)
         for item in expected_functions:
             self.assertIn(item, content)
 
     def test_build_summary(self):
-        import pyroscope_io as pyroscope
-        summary = pyroscope.build_summary().lower()
+        import pyroscope_io as pyro
+        summary = pyro.build_summary().lower()
         self.assertIn("goarch", summary)
         self.assertIn("goos", summary)
         self.assertIn("go version", summary)
